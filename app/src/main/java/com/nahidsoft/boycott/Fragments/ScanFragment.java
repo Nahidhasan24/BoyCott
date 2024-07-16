a9c40fbf91439545b8286f09e21e23fa57a2eedc
Nahidhasan24 <@nahid1122>
2024-07-16 16:49:28 +0600

    second commit

--- app/src/main/java/com/nahidsoft/boycott/Fragments/ScanFragment.java
+++ app/src/main/java/com/nahidsoft/boycott/Fragments/ScanFragment.java
@@ -1,23 +1,51 @@
 package com.nahidsoft.boycott.Fragments;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
 import com.journeyapps.barcodescanner.BarcodeCallback;
 import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+import com.journeyapps.barcodescanner.SourceData;
+import com.journeyapps.barcodescanner.camera.CameraSettings;
+import com.journeyapps.barcodescanner.camera.PreviewCallback;
+import com.nahidsoft.boycott.R;
 import com.nahidsoft.boycott.databinding.FragmentScanBinding;
+
+import java.io.IOException;
+import java.util.EnumMap;
+import java.util.Map;
 
 public class ScanFragment extends Fragment {
 
-    FragmentScanBinding binding;
+    private static final int REQUEST_IMAGE_SELECT = 1;
+
+    private FragmentScanBinding binding;
     private boolean hasScanned = false;
+    private DecoratedBarcodeView barcodeView;
 
     @Nullable
     @Override
@@ -29,11 +57,21 @@ public class ScanFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        barcodeView = view.findViewById(R.id.barcode_scanner);
+
+        RelativeLayout selectImageGallery = view.findViewById(R.id.selectImageGallery);
+        selectImageGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImageFromGallery();
+            }
+        });
+
         startScan();
     }
 
     private void startScan() {
-        binding.barcodeScanner.decodeContinuous(new BarcodeCallback() {
+        barcodeView.decodeContinuous(new BarcodeCallback() {
             @Override
             public void barcodeResult(BarcodeResult result) {
                 if (result.getText() != null && !hasScanned) {
@@ -44,23 +82,72 @@ public class ScanFragment extends Fragment {
         });
     }
 
+    private void selectImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQUEST_IMAGE_SELECT);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_SELECT && resultCode == Activity.RESULT_OK && data != null) {
+            Uri selectedImageUri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), selectedImageUri);
+                scanBarcodeFromBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void scanBarcodeFromBitmap(Bitmap bitmap) {
+        int[] intArray = new int[bitmap.getWidth() * bitmap.getHeight()];
+        bitmap.getPixels(intArray, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        com.google.zxing.RGBLuminanceSource source = new com.google.zxing.RGBLuminanceSource(bitmap.getWidth(), bitmap.getHeight(), intArray);
+        BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+        MultiFormatReader reader = new MultiFormatReader();
+        Map<DecodeHintType, Object> hints = new EnumMap<>(DecodeHintType.class);
+        hints.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
+        reader.setHints(hints);
+
+        try {
+            Result result = reader.decode(binaryBitmap);
+            checkTheResult(result.getText());
+        } catch (NotFoundException e) {
+            Toast.makeText(getActivity(), "No barcode found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     private void checkTheResult(String text) {
-        Toast.makeText(getActivity(), ""+text, Toast.LENGTH_SHORT).show();
-        hasScanned = false;
-        startScan();
+
+        Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT).show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Info!");
+        builder.setMessage(text);
+        builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                hasScanned = false;
+                barcodeView.resume();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
     @Override
     public void onResume() {
         super.onResume();
         hasScanned = false;  // Reset the flag when resuming the fragment
-        binding.barcodeScanner.resume();
+        barcodeView.resume();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        binding.barcodeScanner.pause();
+        barcodeView.pause();
     }
 }
-
